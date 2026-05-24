064c8b82567be48d64c710b03ed3bcb372cc5609
m4rt1m3 <a113572@alunos.uminho.pt>
2026-05-24 17:10:01 +0100

    womp womp faltou o header

--- game/include/move.h
+++ game/include/move.h
@@ -3,7 +3,25 @@
 
 int valida_jogada(EstadoJogo *estado, JOGADA jogada);
 int cor(CARTAS carta);
-int verifica_flags(char flags[32], EstadoJogo *estado, JOGADA jogada);
+int ativa_flags(char flags[32], EstadoJogo *estado, JOGADA jogada);
+int verifica_pilha_origem_aux2(int flag[20], PILHA *origem, JOGADA jogada, int inicio, int fim, CARTAS topo_origem, CARTAS fundo_origem);
+int verifica_pilha_origem_aux(int flag[20],
+                            PILHA *origem,
+                            JOGADA jogada,
+                            int inicio,
+                            int fim,
+                            CARTAS topo_origem,
+                            CARTAS fundo_origem);
+int verifica_pilha_origem(int flag[20],
+                        PILHA *origem,
+                        JOGADA jogada,
+                        int inicio,
+                        int fim,
+                        CARTAS topo_origem,
+                        CARTAS fundo_origem);
+int verifica_pilha_destino_aux2(int flag[20], PILHA *destino, CARTAS topo_origem);
+int verifica_pilha_destino_aux(int flag[20], PILHA *destino, CARTAS topo_origem);
+int verifica_pilha_destino(int flag[20], PILHA *destino, CARTAS topo_origem);
 void joga(EstadoJogo *estado, JOGADA jogada); 
 
 #endif //PATIENCE_MOVE_H
